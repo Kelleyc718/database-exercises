271dc5e07b2aee6594328ab5285daf25cb4da0eb
Chris Kelley <kelleyc718@gmail.com>
2018-03-24 15:20:52 -0500

    add adlister seed and migration to properly named files

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE IF NOT EXISTS albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(50) DEFAULT 'NONE',
+  name VARCHAR(100) NOT NULL,
+  release_date VARCHAR(6) NOT NULL,
+  sales FLOAT NOT NULL,
+  genre VARCHAR(50) DEFAULT 'NONE',
+  PRIMARY KEY (id)
+);
